3f6aec370145c22bda21e49f594f38420553a1f7
Joshua Jacobs-Rebhun <joshua.jacobs.rebhun94@gmail.com>
2022-04-20 22:01:22 +0000

    Created two new packages in the ROS workspace: gps and distance_sensor. The gps package is the ROS node wrapping the GPS driver code that Liz and Matthias wrote. It reads the GPS at a rate of 10 Hz and publishes the latitude and longitude. The distance_sensor package wraps the distance sensor driver and read and publishes distance sensor data at a rate of 5 Hz. More work is needed to get both nodes launched like creating the __init__.py file in the top-level directory of each package.

---
+++ workspace/distance_sensor/distance_sensor.py
@@ -0,0 +1,106 @@
+#! /usr/bin/python3 
+ 
+ 
+#Libraries
+import RPi.GPIO as GPIO
+import time
+import rospy
+from distance_sensor.msg import Distance
+ 
+
+TRIGGER_FRONT = 24
+ECHO_FRONT = 23
+
+TRIGGER_LEFT = 17
+ECHO_LEFT = 27
+         
+TRIGGER_RIGHT = 5
+ECHO_RIGHT = 6
+ 
+def distance(TRIG, ECHO):
+    # set Trigger to HIGH
+    GPIO.output(TRIG, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+# Initializes three distance sensors
+def init_sensors():
+    # GPIO Mode (BOARD / BCM)
+    GPIO.setmode(GPIO.BCM)
+    
+    # Set GPIO pins for front sensor
+    GPIO.setup(TRIGGER_FRONT, GPIO.OUT)
+    GPIO.setup(ECHO_FRONT, GPIO.IN)
+    
+    
+    # Set GPIO pins for left sensor
+    GPIO.setup(TRIGGER_LEFT, GPIO.OUT)
+    GPIO.setup(ECHO_LEFT, GPIO.IN)
+    
+    # Set GPIO pins for right sensor
+    GPIO.setup(TRIGGER_RIGHT, GPIO.OUT)
+    GPIO.setup(ECHO_RIGHT, GPIO.IN)
+ 
+ 
+# Helper function which calls distance to read each sensor
+def read_sensors():
+    dist_front = distance(TRIGGER_FRONT, ECHO_FRONT)
+    dist_left = distance(TRIGGER_LEFT, ECHO_LEFT)
+    dist_right = distance(TRIGGER_RIGHT, ECHO_RIGHT)
+    
+    return dist_front, dist_left, dist_right # Return tuple containing each distance reading
+
+def main():
+	# rate to read distance sensor does not need to be particularly fast
+	distance_read_rate = 5
+	
+	# initialize the node and create the publisher and timer
+	rospy.init_node("distance_sensor_node")
+	distance_publisher = rospy.Publisher("distance", Distance)
+	timer = rospy.Rate(distance_read_rate)
+	
+	try:
+        init_sensors()
+        while not rospy.is_shutdown():
+        	# read sensor data
+            df, dl, dr = read_sensors()
+            
+            # create and fill distance message to be published
+            distances = Distance()
+            distances.distance_front = df
+            distances.distance_left = dl
+            distances.distance_right dr
+            
+            # publish distance 
+            distance_publisher.publish(distances)
+            
+            timer.sleep()
+ 
+	# Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+    
+    
+
---
+++ workspace/gps/nodes/gps_node.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+
+import rospy
+
+
+# os path join to get proper path to gps module in parent directory ??
+import gps
+
+
+if __name__ == "__main__":
+	gps.main()
