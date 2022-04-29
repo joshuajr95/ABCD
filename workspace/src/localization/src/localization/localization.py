a817799032d6adf16bb6642f24f88acdc15acf93
Joshua Jacobs-Rebhun <joshuajacobs-rebhun@dhcp-10-5-4-203.wireless.rochester.edu>
2022-04-28 22:59:54 -0400

    Wrote EKF localization update step. Now I need to debug it and test it on ROSgit status

--- workspace/src/localization/src/localization/localization.py
+++ workspace/src/localization/src/localization/localization.py
@@ -3,9 +3,13 @@
 
 import rospy
 import numpy as np
+import math
+from geopy import distance
+
 from geometry_msgs.msg import Pose
 from gps.msg import gps_data
 from compass.msg import CompassBearing
+from navigation.msg import MotorSpeeds
 
 
 def yaw_to_quaternion(angle):
@@ -19,9 +23,33 @@ def quaternion_to_yaw(quaternion):
 class EKF_Localization:
 	
 	# mu0 and sigma0 must be numpy array and ndarray, respectively
-	def __init__(self, mu0, sigma0, ):
+	# L is the width of the robot (from one tread to the other),
+	# and r is the wheel radius
+	def __init__(self, mu0, sigma0, L, r, alpha, gps_accuracy, mag_accuracy, zero_point):
 		self.mu = mu0
 		self.sigma = sigma0
+		self.L = L
+		self.r = r
+		self.alpha = alpha
+
+		# stores the compass readings received
+		self.compass_bearings = []
+		
+
+		self.Q = np.zeros((3,3))
+		
+		for i in range(2):
+			for j in range(2):
+				self.Q[i][j] = gps_accuracy*gps_accuracy
+		
+		self.Q[2][2] = mag_accuracy*mag_accuracy
+
+		C_inv = np.zeros((3,3))
+		C_inv[0][0] = distance.distance((self.zero_point.latitude,self.gpsdata.longitude), self.zero_point).km*1000.0
+		C_inv[1][1] = distance.distance((self.gpsdata.latitude, self.zero_point.longitude), self.zero_point).km*1000.0
+		C_inv[2][2] = 1
+
+		self.C = np.linalg.inv(C_inv)
 
 
 	# callback function to handle published GPS data
@@ -31,7 +59,7 @@ class EKF_Localization:
 
 	# callback function to handle published compass bearing data
 	def handle_compass(self, compass_bearing):
-		self.compass_bearing = compass_bearing
+		self.compass_bearings.append(compass_bearing)
 	
 
 	# callback function to handle published motor speed data
@@ -40,22 +68,67 @@ class EKF_Localization:
 
 	
 	def step(self, dt):
+		g = np.zeros(3)
+		g[0] = (self.r*dt/2.0)*(self.speeds.left_speed + self.speeds.right_speed)*math.cos(self.mu[2])
+		g[1] = (self.r*dt/2.0)*(self.speeds.left_speed + self.speeds.right_speed)*math.sin(self.mu[2])
+		g[2] = (self.r*dt/self.L)*(self.speeds.right_speed - self.speeds.left_speed)
+
+		mu_bar = self.mu + g
 		
-		if self.speeds is not None:
-			# calculate motion update
-			pass
+		# initialize state variable Jacobian, G
+		G = np.array([1, 0, -(self.r*dt/2.0)*(self.speeds.left_speed + \
+				self.speeds.right_speed)*math.sin(self.mu[2])], \
+				[0, 1, (self.r*dt/2.0)*(self.speeds.left_speed + \
+				self.speeds.right_speed)*math.cos(self.mu[2])], [0, 0, 1])
+		
+		# initialize motion variable Jacobian, V
+		V = np.zeros((2,3))
+		V[0][0] = (self.r*dt/2.0)*math.cos(self.mu[2])
+		V[0][1] = (self.r*dt/2.0)*math.cos(self.mu[2])
+		V[1][0] = (self.r*dt/2.0)*math.sin(self.mu[2])
+		V[1][1] = (self.r*dt/2.0)*math.sin(self.mu[2])
+		V[2][0] = -(self.r*dt)/self.L
+		V[2][1] = (self.r*dt)/self.L
+
+		M = np.zeros((2,2))
+		M[0][0] = self.alpha*self.speeds.left_speed*self.speeds.left_speed + \
+				self.self.alpha*self.speeds.right_speed*self.speeds.right_speed
+		
+		M[1][1] = self.alpha*self.speeds.left_speed*self.speeds.left_speed + \
+				self.self.alpha*self.speeds.right_speed*self.speeds.right_speed
+		
+		sigma_bar = np.matmul(G, np.matmul(self.sigma, np.transpose(G)))
+		sigma_bar += np.matmul(V, np.matmul(M, np.transpose(V)))
+
+
+		# measurement update
+		if self.gpsdata is not None and len(compass_bearing) > 0:
+
+			z = np.zeros(3)
+			z[0] = self.gps_data.longitude
+			z[1] = self.gps_data.latitude
+			z[2] = sum(self.compass_bearings)/len(compass_bearings)
+
+			# compute Kalman gain
+			temp = np.matmul(self.C, np.matmul(sigma_bar, np.transpose(self.C))) + self.Q
+			K = np.matmul(sigma_bar, np.matmul(np.transpose(self.C), temp))
+
+			# compute next mu
+			temp = z - np.dot(self.C, mu_bar)
+			mu_bar += np.matmul(K, temp)
+
+			# compute next sigma
+			identity = np.identity((3,3))
+			temp = identity - np.matmul(K, self.C)
+			sigma_bar = np.matmul(temp, sigma_bar)
+
 		else:
 			pass
 		
-		if self.compass_bearing is not None and self.gpsdata is not None:
-			# calculate measurement update
-			pass
-		else:
-			pass
+		self.mu = mu_bar
+		self.sigma = sigma_bar
 		
-		
-		self.compass_bearing = None
-		self.gpsdata = None
+		self.compass_bearing = []
 		
 		
 
