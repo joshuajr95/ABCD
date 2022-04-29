71ad887be868636bd5eb0dbbdf2c5951299756b8
mhansenp <104087090+mhansenp@users.noreply.github.com>
2022-04-29 00:19:02 -0400

    Update motor_control.py

--- etc/motor_control.py
+++ etc/motor_control.py
@@ -6,7 +6,9 @@ def init_motor():
 					baudrate=9600, 
 					parity=serial.PARITY_NONE,
 					stopbits=serial.STOPBITS_ONE, 
-					bytesize=serial.EIGHTBITS) 
+					bytesize=serial.EIGHTBITS,
+     				write_timeout=0.1) 
+
 	return ser # Return the serial object
 	
 	
@@ -15,9 +17,10 @@ def set_speed(ser, speed_left, speed_right): # Takes in serial object, as well a
 											 # Values NEED to fit in the range 1-255. Value 128 means stop. <128 is reverse. 
 											 # Writing a value of zero (0) will be ignored by the HERO.
 											 
-	ser.flushOutput() # Clear output buffer before transmitting. Shouldn't be necessary tbh	
+	indicator_val = 1	
+	#ser.flushOutput() # Clear output buffer before transmitting. Shouldn't be necessary tbh	
 	
-	ser.write(speed_left.to_bytes(1, 'little'))
-	sleep(0.05)
-	ser.write(speed_right.to_bytes(1, 'little'))
+	ser.write(indicator_val.to_bytes(1, 'big'))
+	ser.write(speed_left.to_bytes(1, 'big'))
+	ser.write(speed_right.to_bytes(1, 'big'))
 	
